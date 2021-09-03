3650a204492ca645d1028249e46c183d1d729e5c
jinjingxuan <jinjingxuan@baidu.com>
2021-09-03 19:59:30 +0800

    upd

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env sh
-
 # 确保脚本抛出遇到的错误
 set -e
 
@@ -9,9 +7,7 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
-
+# git 命令
 git init
 git add .
 git commit -m 'deploy'
