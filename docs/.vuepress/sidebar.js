b6a006b310a9524bccc0b70c3d4aee9c04925087
jinjingxuan <jinjingxuan@baidu.com>
2022-01-06 20:38:18 +0800

    upd

---
+++ docs/.vuepress/sidebar.js
@@ -0,0 +1,255 @@
+module.exports = {
+    '/css/': [{
+        title: 'css',
+        children: [
+            '/css/bfc',
+            '/css/box',
+            '/css/flex',
+            '/css/layout',
+        ]
+    }],
+    '/javascript': [{
+        title: 'javascript',
+        collapsable: true,
+        children: [
+            '/javascript/asynchronous',
+            '/javascript/clone',
+            '/javascript/closure',
+            '/javascript/datatype',
+            '/javascript/date',
+            '/javascript/ES6',
+            '/javascript/number',
+            '/javascript/parse-order',
+            '/javascript/prototype',
+            '/javascript/scope',
+            '/javascript/stringify',
+            '/javascript/this',
+            '/javascript/function',
+        ]
+    }],
+    '/vue': [{
+        title: 'vue',
+        collapsable: true,
+        children: [
+            '/vue/code-1',
+            '/vue/code-2',
+            '/vue/code-3',
+            '/vue/vue3.0-1',
+            '/vue/vue3.0-2',
+            '/vue/vue3.0-3',
+            '/vue/vuex',
+            '/vue/communication',
+            '/vue/responsive',
+            '/vue/template',
+        ]
+    }],
+    '/san': [{
+        title: 'san',
+        collapsable: true,
+        children: [
+            '/san/san',
+            '/san/eslint-plugin-san',
+            '/san/composition',
+        ]
+    }],
+    '/react': [{
+        title: 'react',
+        collapsable: true,
+        children: [
+            '/react/react-1',
+            '/react/react-hooks',
+            '/react/redux',
+        ]
+    }],
+    '/virtual-dom': [{
+        title: 'virtual dom',
+        collapsable: true,
+        children: [
+            '/virtual-dom/virtualdom',
+            '/virtual-dom/example',
+        ]
+    }, ],
+    '/webpack': [{
+        title: 'webpack',
+        collapsable: true,
+        children: [
+            '/webpack/webpack1',
+            '/webpack/webpack2',
+            '/webpack/webpack3',
+            '/webpack/webpack4',
+            '/webpack/webpack5',
+            '/webpack/config',
+            '/webpack/hmr',
+        ]
+    }, ],
+    '/ts': [{
+        title: 'typescript',
+        collapsable: true,
+        children: [
+            '/ts/ts-base',
+            '/ts/ts-advance',
+        ]
+    }, ],
+    '/ssr': [{
+        title: 'ssr',
+        collapsable: true,
+        children: [
+            '/ssr/ssr',
+        ]
+    }, ],
+    '/node': [{
+        title: 'node',
+        collapsable: true,
+        children: [
+            '/node/node-1',
+            '/node/node-2',
+            '/node/node-3',
+            '/node/node-4',
+            '/node/node-5',
+            '/node/node-6',
+        ]
+    }, {
+        title: '深入浅出NodeJs',
+        children: [
+            '/node/book'
+        ]
+    }],
+    '/regular': [{
+        title: '正则表达式',
+        collapsable: true,
+        children: [
+            '/regular/regular',
+        ]
+    }, ],
+    '/mobile': [{
+        title: '移动端问题',
+        collapsable: true,
+        children: [
+            '/mobile/adaptation',
+            '/mobile/long-list',
+        ]
+    }, ],
+    '/algorithm': [{
+        title: '算法',
+        collapsable: true,
+        children: [
+            '/algorithm/array',
+            '/algorithm/dfs-bfs',
+            '/algorithm/dynamic',
+            '/algorithm/greedy',
+            '/algorithm/list',
+            '/algorithm/scene',
+            '/algorithm/sort',
+            '/algorithm/stack',
+            '/algorithm/string',
+            '/algorithm/trace',
+            '/algorithm/tree',
+            '/algorithm/two-pointer',
+        ]
+    }, ],
+    '/design-pattern': [{
+        title: '设计模式',
+        collapsable: true,
+        children: [
+            '/design-pattern/design-pattern',
+            '/design-pattern/object',
+        ]
+    }, ],
+    '/route': [{
+        title: '前端路由',
+        collapsable: true,
+        children: [
+            '/route/route',
+        ]
+    }, ],
+    '/engineering': [{
+        title: '前端工程化',
+        collapsable: true,
+        children: [
+            '/engineering/engineering',
+            '/engineering/eslint',
+            '/engineering/grunt',
+            '/engineering/gulp',
+            '/engineering/rollup',
+        ]
+    }, ],
+    '/safe': [{
+        title: '前端安全',
+        collapsable: true,
+        children: [
+            '/safe/safe',
+        ]
+    }, ],
+    '/module': [{
+        title: '模块化',
+        collapsable: true,
+        children: [
+            '/module/module',
+        ]
+    }, ],
+    '/interview': [{
+        title: '面试题',
+        collapsable: true,
+        children: [
+            '/interview/interview',
+            '/interview/algorithm',
+            '/interview/code-1',
+            '/interview/code-2',
+            '/interview/interview-1',
+            '/interview/interview-2',
+            '/interview/interview-3',
+            '/interview/interview-4',
+            '/interview/interview-5',
+            '/interview/interview-6',
+            '/interview/interview-7',
+            '/interview/interview-8',
+            '/interview/interview-9',
+            '/interview/interview-10',
+            '/interview/interview-11',
+            '/interview/iq',
+        ]
+    }, ],
+    '/chrome': [{
+        title: '浏览器',
+        collapsable: true,
+        children: [
+            '/chrome/gc',
+            '/chrome/render',
+        ]
+    }, ],
+    '/computer': [{
+        title: '计算机基础',
+        collapsable: true,
+        children: [
+            '/computer/computer',
+        ]
+    }, ],
+    '/git': [{
+        title: 'git',
+        collapsable: true,
+        children: [
+            '/git/git',
+        ]
+    }, ],
+    '/code-style': [{
+        title: '代码规范',
+        collapsable: true,
+        children: [
+            '/code-style/code-style',
+        ]
+    }, ],
+    '/tool': [{
+        title: '常用工具',
+        collapsable: true,
+        children: [
+            '/tool/tool',
+        ]
+    }, ],
+    '/read': [{
+        title: '扩展阅读',
+        collapsable: true,
+        children: [
+            '/read/read',
+        ]
+    }, ]
+}
