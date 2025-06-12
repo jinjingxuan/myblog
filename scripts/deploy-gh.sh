9256c4d9316633b2d48b5195d33cf0173561aa04
jinjingxuan <jinjingxuan@baidu.com>
2025-06-12 19:30:47 +0800

    upd

--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -20,7 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，比如我这里就是：
-git push -f git@github.com:jinjingxuan/myblog.git master:gh-pages
+git push -f git@github.com:jinjingxuan/myblog.git main:gh-pages
 
 # 返回上一次的工作目录
 cd -
